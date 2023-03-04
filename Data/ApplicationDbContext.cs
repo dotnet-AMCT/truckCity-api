1b63764adaca734b199b54114816097a54887174
Matias Teruel <matias.teruel@mi.unc.edu.ar>
2023-03-04 04:52:46 -0300

    Entidad Part sin testear, faltan metodos

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,13 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using truckCity_api.Models;
 
 namespace truckCity_api.Data
 {	
 	public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) {}
+
+        public DbSet<Truck> Truck { get; set; }
+        public DbSet<Part> Part { get; set; }
     }
 }
---
+++ Models/Part.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+
+namespace truckCity_api.Models
+{
+    public class Truck
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public List<Part>? Parts { get; set; }
+    }
+
+    public class Part
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required, MaxLength(4), Comment("The code to identify the part")]
+        public string Code { get; set; }
+
+        [Comment("The truck to repair where it's assigned")]
+        public int TruckId { get; set; }
+
+        public Truck? Truck { get; set; }
+
+        public Part(string name, string code, int truckId)
+        {
+            Name = name;
+            Code = code;
+            TruckId = truckId;
+        }
+    }
+}
