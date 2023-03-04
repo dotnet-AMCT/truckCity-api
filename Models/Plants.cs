eb4a79ee5d24be9f919dafa52543ea18b776758a
angelescch <angeles.carrara@mi.unc.edu.ar>
2023-03-03 23:59:06 -0300

    Plants Model

---
+++ Models/Plants.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.SqlServer.Server;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net;
+using System.Xml;
+
+namespace truckCity_api.Models
+{
+    public class Plant
+    {
+        [Key]
+        public int Id { get; private set; }
+
+        [StringLength(450)]
+        [Index(IsUnique = true)]
+        [Required(AllowEmptyStrings = false)]
+        public string Name { get; set; }
+
+        [Required]
+        public int MaxCapacity { get; set; }
+
+        [Required]
+        public int CurrentCapacity { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Address { get; set; } //       (arg format)
+
+        public Plant()
+        {
+
+        }
+    }
+}
