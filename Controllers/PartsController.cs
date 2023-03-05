e87c1fc206f742d02286795025c4641bddcc569f
Matias Teruel <matias.teruel@mi.unc.edu.ar>
2023-03-05 08:22:30 -0300

    Basic CRUD controller for Part entity added, PartModelDTO added but not used

---
+++ Controllers/PartsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using truckCity_api.Data;
+using truckCity_api.Models;
+
+namespace truckCity_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PartsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PartsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Parts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Part>>> GetPart()
+        {
+            return await _context.Part.ToListAsync();
+        }
+
+        // GET: api/Parts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Part>> GetPart(int id)
+        {
+            var part = await _context.Part.FindAsync(id);
+
+            if (part == null)
+            {
+                return NotFound();
+            }
+
+            return part;
+        }
+
+        // PUT: api/Parts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPart(int id, Part part)
+        {
+            if (id != part.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(part).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PartExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Parts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Part>> PostPart(Part part)
+        {
+            _context.Part.Add(part);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPart", new { id = part.Id }, part);
+        }
+
+        // DELETE: api/Parts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePart(int id)
+        {
+            var part = await _context.Part.FindAsync(id);
+            if (part == null)
+            {
+                return NotFound();
+            }
+
+            _context.Part.Remove(part);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PartExists(int id)
+        {
+            return _context.Part.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/DTO/PartModelDTO.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace truckCity_api.Models.DTO
+{
+    public class PartModelDTO
+    {
+        public int Id { get; }
+
+        public PartNames Name { get; }
+
+        public int CodeId { get; }
+
+        public PartCode Code { get; } = null!;
+
+        public int? TruckId { get; }
+
+        public Truck? Truck { get; }
+
+        public PartModelDTO(
+            int id, 
+            PartNames name, 
+            int codeId, 
+            int? truckId)
+        {
+            Id = id;
+            Name = name;
+            CodeId = codeId;
+            TruckId = truckId;
+        }
+    }
+}
